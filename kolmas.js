a34e56d8ef3cb997ee75e690fd7559cb3ef7bf54
Markus Leivo <markus6@ut.ee>
2022-12-05 14:30:03 +0200

    Lisatud piltidele fade-in javascript, mõned vead parandatud

--- kolmas.js
+++ kolmas.js
@@ -19,12 +19,42 @@ r_text[12] = "“Believe in yourself, take on your challenges, dig deep within y
 var i = Math.floor(13*Math.random())    //Valitakse suvaline array ehk tsitaat.
 document.write(r_text[i]);  //Kirjutab footerisse suvaliselt valitud tsitaati.
 
-
 //Footeri nupu JavaScripti alus: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-let mybutton = document.getElementById("myBtn");
-
 // Kui kasutaja vajutab footeris olevale nupule, kerib leht sujuvalt tagasi üles
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTo({ top: 0, behavior: "smooth"}) = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+//Piltide fade-in JavaScripti alus: https://stackoverflow.com/questions/6121203/how-to-do-fade-in-and-fade-out-with-javascript-and-css
+
+// set the initial opacity to 0
+document.getElementById("fade-in").style.opacity = 0;
+
+// use the setInterval function to gradually increase the element's opacity
+setInterval(function() {
+    // get the element
+    var elem = document.getElementById("fade-in");
+    // increase the element's opacity
+    elem.style.opacity = parseFloat(elem.style.opacity) + 0.01;
+    // stop the interval when the element is fully visible
+    if (elem.style.opacity >= 1) {
+      clearInterval(interval);
+    }
+  }, 10);
+
+// set the initial opacity to 0
+document.getElementById("fade-in2").style.opacity = 0;
+
+// use the setInterval function to gradually increase the element's opacity
+setInterval(function() {
+    // get the element
+    var elem = document.getElementById("fade-in2");
+    // increase the element's opacity
+    elem.style.opacity = parseFloat(elem.style.opacity) + 0.01;
+    // stop the interval when the element is fully visible
+    if (elem.style.opacity >= 1) {
+      clearInterval(interval);
+    }
+  }, 10);
