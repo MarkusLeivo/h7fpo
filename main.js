2fa16633e862ffc37a0c8139a10ba56f280c214e
MarkusLeivo <markusleivo20@gmail.com>
2022-12-05 18:08:10 +0200

    Pealeht

---
+++ main.js
@@ -0,0 +1,337 @@
+//Mood testi alus võetud YouTube videost: https://www.youtube.com/watch?v=rCVqQ8NKU2M
+
+function result() {
+    var score=0;
+    if (document.getElementById("correct0").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct1").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct2").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct3").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct4").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct5").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct6").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct7").checked)
+    {
+        score=score+3;
+    }
+    
+    if (document.getElementById("correct8").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct9").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct10").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct11").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct12").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct13").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct14").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct15").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct16").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct17").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct18").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct19").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct20").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct21").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct22").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct23").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct24").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct25").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct26").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct27").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct28").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct29").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct30").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct31").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct32").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct33").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct34").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct35").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct36").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct37").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct38").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct39").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct40").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct41").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct42").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct43").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct44").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct45").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct46").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct47").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct48").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct49").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct50").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct51").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct52").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct53").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct54").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct55").checked)
+    {
+        score=score+3;
+    }
+
+    if (document.getElementById("correct56").checked)
+    {
+        score=score+0;
+    }
+
+    if (document.getElementById("correct57").checked)
+    {
+        score=score+1;
+    }
+
+    if (document.getElementById("correct58").checked)
+    {
+        score=score+2;
+    }
+    
+    if (document.getElementById("correct59").checked)
+    {
+        score=score+3;
+    }
+
+    document.getElementById("tulemus").innerHTML = "Your score is: " +score + " / 45";
+    document.getElementById("lisainfo").innerHTML = "The lower the score, the less you have to worry. This test evaluates on a scale of 0-45, how much anxiety or sadness you might be feeling. If your score is in the upper part of the scale, for example over 35, we suggest seeking help from your friends and family or from trained professionals who can help you get better."
+    //document.write("Your score is:"+score);
+}
+
+//Juhuslike tsitaatide JavaScripti alus võetud siit: https://www.webdevelopersnotes.com/random-text-display-using-javascript-1
+//Tsitaadid: https://www.oberlo.com/blog/motivational-quotes 
+
+//Arrayd on muutujad, millele saab väärtusi anda. Iga tsitaat on justkui üks array.
+var r_text = new Array ();
+r_text[0] = "“All our dreams can come true, if we have the courage to pursue them.” —Walt Disney";
+r_text[1] = "“The secret of getting ahead is getting started.” —Mark Twain";
+r_text[2] = "“It’s hard to beat a person who never gives up.” —Babe Ruth";
+r_text[3] = "“If people are doubting how far you can go, go so far that you can’t hear them anymore.” —Michele Ruiz";
+r_text[4] = "“It’s no use going back to yesterday, because I was a different person then.” ―Lewis Carroll";
+r_text[5] = "“Do what you feel in your heart to be right―for you’ll be criticized anyway.” ―Eleanor Roosevelt";
+r_text[6] = "“You can either experience the pain of discipline or the pain of regret. The choice is yours.” —Unknown";
+r_text[7] = "“If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.” ―Sheryl Sandberg"
+r_text[8] = "“Some people want it to happen, some wish it would happen, others make it happen.” ―Michael Jordan"
+r_text[9] = "“Great things are done by a series of small things brought together.” ―Vincent Van Gogh"
+r_text[10] = "“Keep your eyes on the stars, and your feet on the ground.” ―Theodore Roosevelt"
+r_text[11] = "“Today is your opportunity to build the tomorrow you want.” ―Ken Poirot"
+r_text[12] = "“Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going.” ―Chantal Sutherland"
+var i = Math.floor(13*Math.random())    //Valitakse suvaline array ehk tsitaat.
+document.write(r_text[i]);  //Kirjutab footerisse suvaliselt valitud tsitaati.
+
+
+//Footeri nupu JavaScripti alus: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// Kui kasutaja vajutab footeris olevale nupule, kerib leht sujuvalt tagasi üles
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTo({ top: 0, behavior: "smooth"}) = 0; // For Chrome, Firefox, IE and Opera
+}
